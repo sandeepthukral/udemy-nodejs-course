48bc5bd6813f7da9b4b5665ff833ccf93bb76d2b
Sandeep Thukral <sadeep.thukral@insided.com>
2017-10-26 11:01:08 +0200

    added commands

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
-console.log("Starting app.js");
+#!/usr/bin/env node
+
+console.log('Starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
 
 const notes = require('./notes.js');
+
+const argv = require('yargs').argv;
+
+const command = argv._[0];
+
+console.log('Command ', argv._[0]);
+console.log('yargs ', argv);
+
+if (command === 'add'){
+  notes.addNote(argv.title, argv.body);
+}else if(command === 'list'){
+  notes.listNotes();
+}else if(command === 'read'){
+  notes.getNote(argv.title);
+}else if(command === 'remove'){
+  notes.deleteNote(argv.title);
+} else{
+  console.log('command not recognized');
+}
--- notes.js
+++ notes.js
@@ -1,1 +1,24 @@
 console.log('Starting notes.js');
+
+var addNote = (title, body) => {
+  console.log('Adding note with title', title, ' and body', body);
+}
+
+var listNotes = () => {
+  console.log('Listing notes');
+}
+
+var getNote = (title) => {
+  console.log('Reading note', title);
+}
+
+var deleteNote = (title) => {
+  console.log('Deleting note', title);
+}
+
+module.exports = {
+  addNote,
+  listNotes,
+  getNote,
+  deleteNote
+}
