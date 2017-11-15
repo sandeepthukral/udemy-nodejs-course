685f462ffd103c744ad38693d7f3f502ecf20797
Sandeep Singh Thukral <sandeepthukral@gmail.com>
2017-11-15 21:37:33 +0100

    video 16 - adding non-duplicate notes

--- notes.js
+++ notes.js
@@ -4,7 +4,7 @@ console.log('Starting notes.js');
 const FILE = 'notes-data.json';
 
 var addNote = (title, body) => {
-  console.log('Adding note with title', title, ' and body', body);
+  console.log('Processing note with title', title, ' and body', body);
 
   var notes = [];
 
@@ -12,14 +12,24 @@ var addNote = (title, body) => {
     var notesString = fs.readFileSync(FILE);
     var notes = JSON.parse(notesString);
   } catch (e){
+
   }
+
+  var duplicateTitles = notes.filter( (note) => note.title === title )
 
   var note = {
     title,
     body
   }
-  notes.push(note);
-  fs.writeFileSync(FILE, JSON.stringify(notes));
+
+  if (duplicateTitles.length == 0){
+    notes.push(note);
+    fs.writeFileSync(FILE, JSON.stringify(notes));
+    console.log('Note stored!');
+  } else {
+    console.log("Duplicate entry found. Skipping!");
+  }
+  
 }
 
 var listNotes = () => {
