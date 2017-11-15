8f3d18e1f532c89df97d73f29c8c3744ca01d6c5
Sandeep Thukral <sadeep.thukral@insided.com>
2017-11-15 21:02:46 +0100

    writing to file, deleting note

--- notes.js
+++ notes.js
@@ -1,7 +1,25 @@
+const fs = require('fs');
 console.log('Starting notes.js');
+
+const FILE = 'notes-data.json';
 
 var addNote = (title, body) => {
   console.log('Adding note with title', title, ' and body', body);
+
+  var notes = [];
+
+  try {
+    var notesString = fs.readFileSync(FILE);
+    var notes = JSON.parse(notesString);
+  } catch (e){
+  }
+
+  var note = {
+    title,
+    body
+  }
+  notes.push(note);
+  fs.writeFileSync(FILE, JSON.stringify(notes));
 }
 
 var listNotes = () => {
