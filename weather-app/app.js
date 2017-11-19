d612f45a42189df47f6a3e0361c4b2a1fed7d69b
Sandeep Thukral <sadeep.thukral@insided.com>
2017-11-19 21:59:37 +0100

    video 30 Getting address from google maps api

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,10 @@
 var request = require('request');
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=louis%20paul%20boonstraat%2021%20almere%20netherlands',
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=singel%20421%20amsterdam%20netherlands',
     json: true
   }, (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 2));
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Lat: ${body.results[0].geometry.location.lat}`);
+    console.log(`Lon: ${body.results[0].geometry.location.lng}`);
 });
