36ec118c089d5f1de9f7cedc16680130bf3879b8
Sandeep Thukral <sadeep.thukral@insided.com>
2017-11-19 22:35:27 +0100

    video 32 error checking the callback

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,10 +1,38 @@
 var request = require('request');
 
+const yargs = require('yargs');
+
+var argv = yargs
+  .options({
+    a: {
+      demand:true,
+      alias: 'address',
+      describe: 'Address to fetch westher for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+console.log(argv);
+
+var requestURI = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
+requestURI += encodeURIComponent(argv.a);
+
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=singel%20421%20amsterdam%20netherlands',
-    json: true
-  }, (error, response, body) => {
+  url: requestURI,
+  json: true
+}, (error, response, body) => {
+  if (error){
+    console.log('Unable to connect to Google Service');
+  } else if (body.status === 'OK'){
     console.log(`Address: ${body.results[0].formatted_address}`);
     console.log(`Lat: ${body.results[0].geometry.location.lat}`);
     console.log(`Lon: ${body.results[0].geometry.location.lng}`);
+  } else if (body.status === 'ZERO_RESULTS'){
+    console.log('Unable to find that address');
+  } else {
+    console.log(`This should not get printed ${body.status}`);
+  }
 });
