b40ac59a1e7496bc1528e17342001dc64768227e
Sandeep Thukral <sadeep.thukral@insided.com>
2017-11-19 21:36:59 +0100

    video 29 pretty print json

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,5 +4,5 @@ request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=louis%20paul%20boonstraat%2021%20almere%20netherlands',
     json: true
   }, (error, response, body) => {
-    console.log(body);
+    console.log(JSON.stringify(body, undefined, 2));
 });
