1a69b1c7ac05c161c8c1f9f5b5d7b60ec233780f
Sandeep Singh Thukral <sandeepthukral@gmail.com>
2017-11-26 20:21:43 +0100

    video 36 chaining Promises

--- weather-app/playground/promises.js
+++ weather-app/playground/promises.js
@@ -1,14 +1,36 @@
-var somePromise = new Promise((resolve, reject)=> {
-    setTimeout(() => {
-        // resolve('Hey. It worked!');
-        reject('Unable to fulfill promise');
-    }, 2500);
-    
-});
-
-somePromise.then(
-    (message) => {
-        console.log(`Success ${message}`);
-    }, (errorMessage) => {
-        console.log(`Error: ${errorMessage}`);
+var asyncAdd = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (typeof a === 'number' && typeof b === 'number') {
+                resolve(a+b);
+            } else {
+                reject();
+            }}, 1500);
     });
+};
+
+
+asyncAdd(3,4).then((result) => {
+        console.log(result);
+        return asyncAdd(result, 33);
+    }, (error) => {
+        console.log(error);
+    }).then((result) => {
+        console.log(`Result: ${result}`);
+    }, (error) => {
+        console.log(`Error ${error}`);
+    });
+// var somePromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         // resolve('Hey. It worked!');
+//         reject('Unable to fulfill promise');
+//     }, 2500);
+    
+// });
+
+// somePromise.then(
+//     (message) => {
+//         console.log(`Success ${message}`);
+//     }, (errorMessage) => {
+//         console.log(`Error: ${errorMessage}`);
+//     });
