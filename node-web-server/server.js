d7465f3feb3194d40007f5bd8e45e9624f97a3fd
Sandeep Thukral <sadeep.thukral@insided.com>
2017-11-27 22:28:23 +0100

    video 44 handlebars

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,19 +1,34 @@
+const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 
 var app = express();
-
+hbs.registerPartials(path.resolve(__dirname , 'views/partials'));
+// hbs.registerPartials((__dirname + '/views/partials'));
+app.set('view engine', hbs);
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (input) => {
+  return input.toUpperCase();
+});
 
 app.get('/', (req, res) => {
   // res.send('Hello! express');
-  res.send({
-    "name": "Sourcy",
-    "likes": ["Biking", "Baking"]
+  res.render('home.hbs', {
+    pageTitle: 'Root page',
+    welcomeMessage: 'Welcome to my website'
   });
 });
 
 app.get('/about', (req, res) => {
-  res.send('About Page');
+  res.render('about.hbs', {
+    pageTitle: 'About page',
+    currentYear: new Date().getFullYear()
+  });
 });
 
 app.get('/bad', (req, res) => {
