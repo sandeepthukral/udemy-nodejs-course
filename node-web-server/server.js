fbc11fc26de6796a3ba0262102c1a95527282645
Sandeep Thukral <sadeep.thukral@insided.com>
2017-11-28 18:28:28 +0100

    video 45 Express middleware

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,12 +1,30 @@
 const path = require("path");
 const express = require("express");
 const hbs = require("hbs");
+const fs = require('fs');
 
 var app = express();
 hbs.registerPartials(path.resolve(__dirname , 'views/partials'));
 // hbs.registerPartials((__dirname + '/views/partials'));
 app.set('view engine', hbs);
-app.use(express.static(__dirname + '/public'));
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+//   // We are not calling next(), so this is where it all stops
+// });
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  const now = new Date().toString();
+
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', `${log} \n`, (err) => {
+    if (err) console.log('Unable to log to file');
+  });
+  next();
+});
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
