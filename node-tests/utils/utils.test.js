6243630b4d526ecb924f1d7616c3b71682427df6
Sandeep Thukral <sadeep.thukral@insided.com>
2017-12-01 22:50:17 +0100

    video 53 assertion library

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,17 +1,23 @@
-const utils = require('./utils');
+const expect = require("expect");
+const utils = require("./utils");
 
 it('should add two numbers', () => {
   var res = utils.add(3,7);
   const expected = 10;
-  if (res !== expected) {
-    throw new Error(`Expected ${expected} but got ${res}`);
-  }
+  expect(res).toBe(expected).toBeA('number');
 });
 
 it('should square a number', () => {
   var res = utils.square(7);
   const expected = 49;
-  if (res !== expected) {
-    throw new Error(`Expected ${expected} but got ${res}`);
-  }
+  expect(res).toBe(expected).toBeA('number');
+});
+
+it('should verify first and last names are set', () => {
+  var user = utils.setName({}, "First Last");
+  expect(user).toInclude({
+    firstName: "First"
+  }).toInclude({
+    lastName: "Last"
+  });
 });
