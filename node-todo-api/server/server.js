39d7fc894c9afc0b33e0ba6015b27bbe1b174bcb
Sandeep Singh Thukral <sandeepthukral@gmail.com>
2017-12-10 21:43:14 +0100

    video 71 validations

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -9,24 +9,46 @@ mongoose.connect(db_url, {
 
 var Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
   }, 
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 });
 
 var newTodo = new Todo({
-  text: 'Eat Dinner',
-  completed: true,
-  completedAt: 12345678
+  text: '   Edit this video   '
 });
 
-newTodo.save().then((doc) => {
-  console.log(`Saved todo\n ${doc}`);
+// newTodo.save().then((doc) => {
+//   console.log(`Saved todo\n ${doc}`);
+// }, (err) => {
+//   console.log(`Unable to save the Todo\n ${err}`);
+// });
+
+var User = mongoose.model('User', {
+  email: {
+    required: true,
+    type: String,
+    minLength: 1,
+    trim: true
+  }
+});
+
+var newUser = new User({
+  email: ' java  '
+});
+
+newUser.save().then((doc) => {
+  console.log(JSON.stringify(doc, undefined, 2));
 }, (err) => {
-  console.log('Unable to save the Todo');
+  console.log(`Unable to save the document.\n ${err}`);
 });
