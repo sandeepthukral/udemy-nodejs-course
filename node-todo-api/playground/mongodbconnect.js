d2a47e7b3d9acc1f124c17f642cf8043c6c11be0
Sandeep Singh Thukral <sandeepthukral@gmail.com>
2017-12-10 17:11:34 +0100

    video 65 quering

--- node-todo-api/playground/mongodbconnect.js
+++ node-todo-api/playground/mongodbconnect.js
@@ -1,4 +1,4 @@
-const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   if (err){
@@ -6,35 +6,72 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   }
   console.log('Connected to MongoDB Server');
 
-  // Insert a new doc to collection Todos (task, completed boolean)
-  const collection = db.db('TodoApp').collection('Todos');
-  const firstTask = {
-    text: 'All Todos',
-    completed: false
-  };
+  // const collection = db.db('TodoApp').collection('Todos');
 
-  collection.insertOne(firstTask, (err, res) => {
-    if (err) {
-      return console.log('Unable to insert data\n', err);
-    }
-    console.log(JSON.stringify(res.ops, undefined, 2));
-  });
+  // collection.find({
+  //   completed: false
+  // }).toArray().then((docs) => {
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch docs from DB');
+  // });
 
-  // Insert a new doc to collection 'Users' (name, age location) 
-  const firstUser = {
-    name: 'Andrew Jackson',
-    are: 99,
-    location: 'Home in Java Land'
-  };
+  // collection.find({
+  //   _id: new ObjectID('5a2d106a4f4a701962308524')
+  // }).toArray().then((docs) => {
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch docs from DB');
+  // });
+
+
+  // collection.find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`)
+  // }, (err) => {
+  //   console.log('Unable to fetch docs from DB');
+  // });
 
   const collection = db.db('TodoApp').collection('Users');
 
-  collection.insertOne(firstUser, (err, res) => {
-    if (err) {
-      return console.log('Unable to insert data\n', err);
-    }
-    console.log(JSON.stringify(res.ops, undefined, 2));
+  collection.find({
+    name: 'Andrew Jackson'
+  }).toArray().then((docs) => {
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('Unable to fetch docs from DB');
   });
 
-  db.close();
+  // // Insert a new doc to collection Todos (task, completed boolean)
+  // const collection = db.db('TodoApp').collection('Todos');
+  // const firstTask = {
+  //   text: 'All Todos',
+  //   completed: false
+  // };
+
+  // collection.insertOne(firstTask, (err, res) => {
+  //   if (err) {
+  //     return console.log('Unable to insert data\n', err);
+  //   }
+  //   console.log(JSON.stringify(res.ops, undefined, 2));
+  // });
+
+  // Insert a new doc to collection 'Users' (name, age location) 
+  // const firstUser = {
+  //   name: 'Andrew Jackson',
+  //   age: 99,
+  //   location: 'Home in Java Land'
+  // };
+
+  // const collection = db.db('TodoApp').collection('Users');
+
+  // collection.insertOne(firstUser, (err, res) => {
+  //   if (err) {
+  //     return console.log('Unable to insert data\n', err);
+  //   }
+  //   console.log(JSON.stringify(res.ops, undefined, 2));
+  //   console.log(res.ops[0]._id.getTimestamp());
+  // });
+
+  // db.close();
+  // console.log('Conection closed');
 });
