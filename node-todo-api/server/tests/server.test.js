7ec5ca1fac863c623b71e2e5c467fb9618506c90
Sandeep Singh Thukral <sandeepthukral@gmail.com>
2017-12-24 10:16:31 +0100

    video 76 testing the GET /todos

--- node-todo-api/server/tests/server.test.js
+++ node-todo-api/server/tests/server.test.js
@@ -4,11 +4,20 @@ const request = require("supertest");
 const {app} = require("./../server");
 const {Todo} = require("./../models/todo");
 
+const dummyTodos = [{
+  text: "First todo"
+},{
+   text: "Second todo"
+}];
+
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  Todo
+    .remove({})
+    .then(() => Todo.insertMany(dummyTodos))
+    .then(() => done());
 });
 
-describe('post /todos', () => {
+describe('POST /todos', () => {
   it('should create a new todo', (done) => {
     var text = 'Test todo text';
     request(app)
@@ -22,7 +31,7 @@ describe('post /todos', () => {
         if (err) {
           return done(err);
         }
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -41,10 +50,23 @@ describe('post /todos', () => {
           return done(err);
         }
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
         })
         .catch((e) => done(e));
       });
+  });
+});
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .send()
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
   });
 });
